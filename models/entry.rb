94dc842f529eb5d609d174177b7d5a0f8a22005b
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-10-17 18:46:41 -0700

    add eq test and initialize attributes

--- models/entry.rb
+++ models/entry.rb
@@ -1,4 +1,10 @@
 class Entry
   attr_accessor :name, :phone_number, :email
 
+  def initialize(name, phone_number, email)
+    @name = name
+    @phone_number = phone_number
+    @email = email
+  end
+
 end
