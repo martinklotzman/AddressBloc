d876593b7eecc9f77c2f1faa08c7e23211fe6433
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-10-17 19:27:57 -0700

    AddressBook class and attributes test

---
+++ spec/address_book_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../models/address_book'
+
+RSpec.describe AddressBook do
+  describe "attributes" do
+    it "responds to entries" do
+      book = AddressBook.new
+      expect(book).to respond_to(:entries)
+    end
+  end
+end
