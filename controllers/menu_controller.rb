162ac74fddd45186c3d57aae18e8a14b7459f65b
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2017-10-18 19:04:03 -0700

    create_entry method

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -49,6 +49,20 @@ class MenuController
   end
 
   def create_entry
+    system "clear"
+    puts "New AddressBloc Entry"
+
+    print "Name: "
+    name = gets.chomp
+    print "Phone number: "
+    phone = gets.chomp
+    print "Email: "
+    email = gets.chomp
+
+    address_book.add_entry(name, phone, email)
+
+    system "clear"
+    puts "new entry created"
   end
 
   def search_entries
